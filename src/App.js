050f366d7c3945b7695cd2a47fb2d06e19aa28e9
anselmotadeu <anselmotadeu@outlook.com>
2024-11-05 08:05:33 -0300

    ✨ feat: adicionar cabeçalho e rodapé ao layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Container, Button, Typography, Paper, TextField } from '@mui/material';
+import { AppBar, Toolbar, Typography, Container, Button, Paper, TextField } from '@mui/material';
 import { Controlled as CodeMirror } from 'react-codemirror2';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
@@ -27,35 +27,47 @@ function App() {
   };
 
   return (
-    <Container>
-      <Typography variant="h4" gutterBottom>CodeGuardian</Typography>
-      <TextField
-        label="Linguagem Detectada"
-        value={language}
-        fullWidth
-        margin="normal"
-        InputProps={{
-          readOnly: true,
-        }}
-      />
-      <CodeMirror
-        value={code}
-        options={{
-          mode: language.toLowerCase(),
-          theme: 'material',
-          lineNumbers: true
-        }}
-        onBeforeChange={(editor, data, value) => {
-          setCode(value);
-        }}
-      />
-      <Button variant="contained" color="primary" onClick={analyzeCode} style={{ marginTop: '10px' }}>
-        Analisar Código
-      </Button>
-      <Paper elevation={3} style={{ whiteSpace: 'pre-wrap', marginTop: '20px', padding: '20px' }}>
-        <div dangerouslySetInnerHTML={{ __html: result }} />
-      </Paper>
-    </Container>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            CodeGuardian
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <Typography variant="h4" gutterBottom>CodeGuardian</Typography>
+        <TextField
+          label="Linguagem Detectada"
+          value={language}
+          fullWidth
+          margin="normal"
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        <CodeMirror
+          value={code}
+          options={{
+            mode: language.toLowerCase(),
+            theme: 'material',
+            lineNumbers: true
+          }}
+          onBeforeChange={(editor, data, value) => {
+            setCode(value);
+          }}
+        />
+        <Button variant="contained" color="primary" onClick={analyzeCode} style={{ marginTop: '10px' }}>
+          Analisar Código
+        </Button>
+        <Paper elevation={3} style={{ whiteSpace: 'pre-wrap', marginTop: '20px', padding: '20px' }}>
+          <div dangerouslySetInnerHTML={{ __html: result }} />
+        </Paper>
+      </Container>
+      <footer style={{ textAlign: 'center', padding: '10px', marginTop: '20px', backgroundColor: '#f8f9fa' }}>
+        <Typography variant="body2" color="textSecondary">© {new Date().getFullYear()} CodeGuardian. Todos os direitos reservados.</Typography>
+      </footer>
+    </>
   );
 }
 
